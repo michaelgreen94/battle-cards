4d77e200fd1aa8736c8b2ef42d074a71d75d5b67
michael <michael.green94@hotmail.com>
2018-08-31 00:19:23 -0600

    styling battlefield

--- src/components/Enemy.vue
+++ src/components/Enemy.vue
@@ -1,12 +1,28 @@
-<template>
+<!-- <template>
   <div class="enemy">
-    <div class="e-visible">
+    <div class="visible">
       <div class="e-card" style="" v-for="(target, key) in enemy.hand" v-if="enemy.hand[key].visible" @click="enemyCard(enemy.id, enemy.hand[key].id)">
         <img class="v-img" :src="enemy.hand[key].img" alt="">
       </div>
       <div class="hidden" v-else>
         <img class="h-img" src="../assets/gamecard2Cropped.jpg" alt="" @click="enemyCard(enemy.id, enemy.hand[key].id)">
       </div>
+    </div>
+  </div>
+</template> -->
+
+<template>
+  <div class="enemy">
+    <div class="e-card" style="" v-for="(target, key) in enemy.hand" v-if="enemy.hand[key].visible" @click="enemyCard(enemy.id, enemy.hand[key].id)">
+      <div class="stats"><i class="fas fa-bolt"></i> {{enemy.hand[key].attack}}</div>
+      <div class="stats"><i class="fas fa-shield-alt"></i> {{enemy.hand[key].defense}}</div>
+      <div class="stats"><i class="fas fa-heart"></i> {{enemy.hand[key].health}}</div>
+      <img class="card-img" :src="enemy.hand[key].img" alt="">
+      <div class="name">{{enemy.hand[key].name}}</div>
+    </div>
+    <div class="hidden" v-else>
+      <div class="h-img" @click="enemyCard(enemy.id, enemy.hand[key].id)"></div>
+      <!-- <img class="h-img" src="../assets/gamecard2Cropped.jpg" alt="" @click="enemyCard(enemy.id, enemy.hand[key].id)"> -->
     </div>
   </div>
 </template>
@@ -41,6 +57,69 @@
 </script>
 
 <style>
+  .enemy {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+
+  .e-card {
+    width: 270px;
+    height: 400px;
+    background: linear-gradient(to top, #3a0704, #a51715, white);
+    transition: box-shadow .2s;
+    border-radius: 15px;
+  }
+
+  .h-img {
+    width: 270px;
+    height: 400px;
+    background-image: url('../assets/gamecard.gif');
+    background-size: cover;
+    background-position: center;
+    border: 1px solid white;
+    border-radius: 15px;
+  }
+
+  .h-img:hover {
+    box-shadow: 0px 0px 20px #ffffffb3;
+    cursor: pointer;
+  }
+
+  .e-card:hover {
+    box-shadow: 0px 0px 20px #ffffffb3;
+    cursor: pointer;
+  }
+
+  .name {
+    height: 50px;
+    width: 270px;
+    background-color: #00000080;
+    line-height: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+    flex-wrap: wrap;
+    color: white;
+  }
+
+  .card-img {
+    width: 100%;
+  }
+
+  .stats {
+    display: flex;
+    font-size: 20px;
+    font-weight: bold;
+    transform: translate(5%, 35%);
+    color: black;
+  }
+
   /* .visible {
     display: flex;
     flex-direction: row;
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="player">
-    <div class="p-visible">
-      <div class="p-card" style="" v-for="(target, key) in player.hand" @click="playerCard(player.id, player.hand[key].id)">
-        <img class="p-img" :src="player.hand[key].img" alt="">
-      </div>
+    <div class="card" style="" v-for="(target, key) in player.hand" @click="playerCard(player.id, player.hand[key].id)">
+      <div class="stats"><i class="fas fa-bolt"></i> {{player.hand[key].attack}}</div>
+      <div class="stats"><i class="fas fa-shield-alt"></i> {{player.hand[key].defense}}</div>
+      <div class="stats"><i class="fas fa-heart"></i> {{player.hand[key].health}}</div>
+      <img class="card-img" :src="player.hand[key].img" alt="">
+      <div class="name">{{player.hand[key].name}}</div>
     </div>
   </div>
 </template>
@@ -38,6 +40,66 @@
 </script>
 
 <style>
+  .player {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+
+  .card {
+    width: 270px;
+    height: 400px;
+    background: linear-gradient(to top, #41b883, #50e09f, white);
+    transition: box-shadow .2s;
+    border-radius: 15px;
+  }
+
+
+
+  .card:hover {
+    box-shadow: 0px 0px 20px #ffffffb3;
+    cursor: pointer;
+  }
+
+  .name {
+    height: 50px;
+    width: 270px;
+    background-color: #00000080;
+    line-height: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+    flex-wrap: wrap;
+    color: white;
+  }
+
+  .card-img {
+    width: 100%;
+  }
+
+  .stats {
+    display: flex;
+    font-size: 20px;
+    font-weight: bold;
+    transform: translate(5%, 35%);
+  }
+
+  /* .card-header img {
+    height: 100%;
+    display: block;
+  } */
+
+
+
+
+
+
+
   /* .visible {
     display: flex;
     flex-direction: row;
